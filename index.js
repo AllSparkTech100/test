ff5ca4dfb9fc6ea2f6eaec8562ec40c9a7e1b224
AllSparkTech100 <105206727+AllSparkTech100@users.noreply.github.com>
2023-08-14 13:08:26 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var images = document.querySelectorAll('.slider img');
-var currentIndex = 0;
-var interval = setInterval(changeImage, 4000); // Change image every 4 seconds
-
-function changeImage() {
-  images[currentIndex].style.display = 'none';
-  currentIndex = (currentIndex + 1) % images.length;
-  images[currentIndex].style.display = 'block';
-};
