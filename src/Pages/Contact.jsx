3c852a6697549d98e73009ab2f1466671602ca75
AllSparkTech100 <105206727+AllSparkTech100@users.noreply.github.com>
2024-02-16 11:47:58 +0100

    Add files via upload

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -16,27 +16,57 @@ function Contact() {
           </div>
         </div>
 
-        <Container>
-          <Container>
+          <Container fluid>
             <div className="inner_info">
               <h2>Contact Information</h2>
             </div>
-            <Row>
-              <Col>
-                <p>
+            <Row className="last-contact-section" gap={1}>
+              <Col md={3} sm={12}>
+                <p className="fs-5">
                   Address: 198 West 21th Street, Suite 721 New York NY 10016
                 </p>
               </Col>
-              <Col>
-                <p>
-                  Phone: <span><a href="tel:+2349027209415" className="text-decoration-none">+ 2349027209415</a></span>
+              <Col md={3} sm={12}>
+                <p className="fs-5">
+                  Phone:&nbsp;
+                  <span>
+                    <a
+                      href="tel:+2349027209415"
+                      className="text-decoration-none"
+                    >
+                      + 2349027209415
+                    </a>
+                  </span>
                 </p>
               </Col>
-              <Col></Col>
-              <Col></Col>
+              <Col md={3} sm={12}>
+                <p className="fs-5">
+                  Email:&nbsp;
+                  <span>
+                    <a
+                      href="mailto:yoursite@gmail.com"
+                      className="text-decoration-none"
+                    >
+                      yoursite@gmail.com
+                    </a>
+                  </span>
+                </p>
+              </Col>
+              <Col md={3} sm={12}>
+                <p className="fs-5">
+                  Phone:&nbsp;
+                  <span>
+                    <a
+                      href="http://yoursite.com"
+                      className="text-decoration-none"
+                    >
+                      yoursite.com
+                    </a>
+                  </span>
+                </p>
+              </Col>
             </Row>
           </Container>
-        </Container>
       </main>
     </>
   );
