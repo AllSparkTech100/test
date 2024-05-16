747e71dcdf815e5c309d18fbaa61fe102f5ba69d
Alexander Jr <105206727+AllSparkTech100@users.noreply.github.com>
2024-05-16 17:59:55 +0100

    Making Changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './app.scss';
+import "./app.scss";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
@@ -9,10 +9,12 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-    <ThemeProvider breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
-  minBreakpoint="xxs">
-    <App />
-    </ThemeProvider>
+      <ThemeProvider
+        breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
+        minBreakpoint="xxs"
+      >
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
