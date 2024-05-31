fde7f4a21de179b0b99a4eacf1f363a5c149220e
Alexander Jr <105206727+AllSparkTech100@users.noreply.github.com>
2024-05-31 18:19:32 +0100

    Additional Updates

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -29,46 +29,61 @@ function About() {
         {/* Info Sect */}
         <Container fluid className="rest_carrier h-100">
           {/* About */}
-          <Row className=" rest_info m-1">
-            <Col sm={12} md={7} xl={6} className="two_img mt-2">
-              <Image src={barr} alt="" className="" rounded />
-            </Col>
-            <Col sm={6} md={5} xl={6} className="tp">
-              <h6 className="fw-normal position-relative text-capitalize ">
-                about
-              </h6>
-              <h3 className="fw-medium brand_name">Feliciano Restaurant</h3>
-              <p className="fw-normal mt-3 fs-4">
-                A small river named Duden flows by their place and supplies it
-                with the necessary regelialia. It is a paradisematic country, in
-                which roasted parts of sentences fly into your mouth.
-              </p>
-              <p className="fw-normal fs-4 mt-4">
-                Mon -Fri &nbsp; <span className="fw-bold">8AM - 11PM</span>
-              </p>
-              <h4 className="fw-bold fs-1">+234-9027-91057</h4>
-            </Col>
-          </Row>
+          <div className="container-fluid mt-5">
+            <div className="container-sm-fluid">
+              <Row className=" show_case">
+                <Col>
+                  <div className="double_images">
+                    <div className="item_1">
+                      <Image src={chef} alt="" />
+                    </div>
+                    <div className="item_2">
+                      <Image src={barr} alt="" />
+                    </div>
+                  </div>
+                </Col>
+                <Col>
+                  <div className="shop_time">
+                    <h6 className="fw-normal brand_header">about</h6>
+                    <h3 className="fw-medium brand_name ">
+                      Feliciano Restaurant
+                    </h3>
+                    <p className="fw-normal fs-4 ">
+                      A small river named Duden flows by their place and
+                      supplies it with the necessary regelialia. It is a
+                      paradisematic country, in which roasted parts of sentences
+                      fly into your mouth.
+                    </p>
+                    <p className="fw-normal fs-4 mt-4">
+                      Mon -Fri &nbsp;{" "}
+                      <span className="fw-bold">8AM - 11PM</span>
+                    </p>
+                    <h4 className="fw-bold thime fs-1">+234-9027-91057</h4>
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          </div>
 
           {/* Personnel Count */}
           <Row className="stat p-md-5 text-md-start text-center">
-            <Col md={3} lg={2} sm={12} className="mb-4">
+            <Col md={2} sm={12} className="mb-4">
               <h3 className="fw-semibold">18</h3>
               <h6>YEARS OF EXPERIENCE</h6>
             </Col>
-            <Col md={3} lg={2} sm={12} className="mb-4">
+            <Col md={2} sm={12} className="mb-4">
               <h3 className="fw-semibold">100</h3>
               <h6>MENUS / DISHES</h6>
             </Col>
-            <Col md={3} lg={2} sm={12} className="mb-4">
+            <Col md={2} sm={12} className="mb-4">
               <h3 className="fw-semibold">50</h3>
               <h6>STAFFS</h6>
             </Col>
-            <Col md={3} lg={2} sm={12} className="mb-4">
+            <Col md={2} sm={12} className="mb-4">
               <h3 className="fw-semibold">15,000</h3>
               <h6>HAPPY CUSTOMERS</h6>
             </Col>
-            <Col md={6} lg={4} sm={12} className="mb-4 text-md-center ">
+            <Col md={4} sm={12} className="mb-4">
               <p className="fs-5 text-center text-md-start ">
                 A small river named Duden flows by their place and supplies it
                 with the necessary regelialia.
@@ -109,21 +124,21 @@ function About() {
 
         {/* Chefs */}
         <Container fluid>
-          <Container>
+          <Container fluid="md">
             <section className="chef">
-              <Col sm={12} md={12} className="text-center head">
+              <Col sm={12} md={12} className="text-center chef_head">
                 <h4>Chef</h4>
                 <h2 className="fw-bold">Our Master Chef</h2>
               </Col>
               <Row id="imges">
-                <Col md={6} lg={3} sm={12}>
+                <Col md={3} sm={12} className="mt-sm-5">
                   <ImgCard
                     imgUrl={chef}
                     name={"John Smooth"}
                     text={"Restaurant Owner"}
                   />
                 </Col>
-                <Col md={6} lg={3} sm={12}>
+                <Col md={3} sm={12} className="mt-sm-5">
                   <ImgCard
                     imgUrl={
                       "https://www.shutterstock.com/image-photo/cooking-culinary-people-concept-happy-600nw-2136448507.jpg"
@@ -132,7 +147,7 @@ function About() {
                     text={"Head Chef"}
                   />
                 </Col>
-                <Col md={6} lg={3} sm={12}>
+                <Col md={3} sm={12} className="mt-sm-5">
                   <ImgCard
                     imgUrl={
                       "https://dg1xqmhtoint1.cloudfront.net/img/ihm/blog/ihm-12.webp?mtime=20220519155503&focal=none"
@@ -141,7 +156,7 @@ function About() {
                     text={"Chef"}
                   />
                 </Col>
-                <Col md={6} lg={3} sm={12}>
+                <Col md={3} sm={12} className="mt-sm-5">
                   <ImgCard
                     imgUrl={
                       "https://www.ecpi.edu/sites/default/files/Aug%2022%20CIV.png"
@@ -154,6 +169,7 @@ function About() {
             </section>
           </Container>
         </Container>
+        {/* Form */}
         <Container fluid className="bg-image form_bg p-md-5">
           <div>
             <FormComp />
@@ -161,16 +177,16 @@ function About() {
         </Container>
 
         {/* Testimony */}
-        <Container className="testimony">
+        <Container fluid className="testimony">
           <Col sm={12} md={12} className="text-center infoo">
             <h4>Testimony</h4>
-            <div className="infoo_inner">
-              <h2 className="fw-bold">Happy Customers</h2>
+            <div className="infoo_inner text-center">
+              <h2 className="fw-bold text-center">Happy Customers</h2>
             </div>
           </Col>
           <Container fluid>
-          <Test />
-        </Container>
+            <Test />
+          </Container>
         </Container>
 
         {/* Blog */}
